85fb3577de3b2319cdea74dd09d45c0485e215f3
morfiy <edurnev@keaz.ru>
2019-07-09 10:07:17 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:0,
+    nav:true,
+    autoplay:false,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('autoplay.play.owl',[3000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('autoplay.stop.owl')
+})
+$(document).ready(function(){
+    $(".navbar-nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+$(document).ready(function(){
+    $(".footer-links").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
