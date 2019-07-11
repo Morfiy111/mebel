9cf9bf504233ee883397862e2c7f701b770dfed9
morfiy <edurnev@keaz.ru>
2019-07-11 09:54:14 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
+var itemsCount=3;
+if (window.matchMedia("(max-width: 991px)").matches) {
+    itemsCount=2;
+}
+if (window.matchMedia("(max-width: 767px)").matches) {
+    itemsCount=1;
+}
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-    items:1,
+    items:itemsCount,
     loop:true,
     margin:0,
     nav:true,
-    autoplay:false,
+    autoplay:true,
     autoplayTimeout:3000,
     autoplayHoverPause:true
 });
